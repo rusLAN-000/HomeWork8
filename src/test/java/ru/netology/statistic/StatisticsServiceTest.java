574ed4a1aa632463f654a20e85c00028cd730996
rusLAN <puzo00712@gmail.com>
2023-06-17 15:33:34 +0300

    initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,15 +18,5 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void NextFindMax() {
-        StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11,};
-        long expected = 11;
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
